e3ba82199d496d7a8c314d680cd6b139899fc484
Sergey Chirva <sword76@users.noreply.github.com>
2024-10-22 15:49:49 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import telebot
 import os
 from main import bot
 
-# Import 
+# Import chat users
 TG_BOT_CHATS = os.getenv("TG_BOT_CHATS").lower().split(",")
 
 # The bot entery point. Message loading, converrtin from JSON, check message format
@@ -16,7 +16,10 @@ def handler(event, context):
         update.message is not None
         and update.message.from_user.username.lower() in TG_BOT_CHATS
     ):
-        bot.process_new_updates([update])
+        try:
+            bot.process_new_updates([update])
+        except Exception as e:
+            print(e)
 
     return {
         "statusCode": 200,
